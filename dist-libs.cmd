3ee4c169f540f61b681d43176377d67336af7347
tul-urte <tul_urte@brentcroft.com>
2018-08-01 22:43:53 +0100

    Clean up 4

--- dist-libs.cmd
+++ dist-libs.cmd
@@ -2,8 +2,15 @@ SETLOCAL
 
 set root=.\
 
-CALL :distributeLibs dist\
+:: update cucumber resources - feedback to build tests
 CALL :distributeLibs gui-driver-cucumber\src\test\resources\lib\
+
+:: prepare remote and inspector packs
+CALL :distributeLibsRemote dist\remote
+CALL :distributeLibs dist\inspector
+
+:: update demo froom cucumber
+xcopy /S /Y %root%gui-driver-cucumber\src\test\resources dist\demo
 
 if errorlevel 1 pause
 
@@ -18,12 +25,25 @@ EXIT /B %ERRORLEVEL%
 :distributeLibs
 xcopy /Y %root%gui-driver-cucumber\target\*.jar %1
 xcopy /Y %root%gui-driver-adapter\target\*.jar %1
+xcopy /Y %root%gui-driver-adapter-fx\target\*.jar %1
 xcopy /Y %root%gui-driver-adapter-swt\target\*.jar %1
 xcopy /Y %root%gui-driver-browser\target\*.jar %1
 xcopy /Y %root%gui-driver-inspector\target\*.jar %1
-
 xcopy /Y %root%swingset2\target\*.jar %1
 xcopy /Y %root%swtset2\target\*.jar %1
+
+
+:: a function to distribute libs
+:: from various places
+:: to a target directory
+:distributeLibsRemote
+xcopy /Y %root%gui-driver-driver\target\*.jar %1
+xcopy /Y %root%gui-driver-adapter\target\*.jar %1
+xcopy /Y %root%gui-driver-adapter-fx\target\*.jar %1
+xcopy /Y %root%gui-driver-adapter-swt\target\*.jar %1
+xcopy /Y %root%gui-driver-events\target\*.jar %1
+xcopy /Y %root%gui-driver-utils\target\*.jar %1
+
 
 
 EXIT /B 0
