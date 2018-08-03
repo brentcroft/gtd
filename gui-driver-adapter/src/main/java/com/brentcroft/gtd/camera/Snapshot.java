493098b3a5f41abc2416c52d3ed5d8d9abad97de
tul-urte <tul_urte@brentcroft.com>
2018-08-03 07:49:34 +0100

    Remove coupling to FX package.

--- gui-driver-adapter/src/main/java/com/brentcroft/gtd/camera/Snapshot.java
+++ gui-driver-adapter/src/main/java/com/brentcroft/gtd/camera/Snapshot.java
@@ -1,54 +1,20 @@
 package com.brentcroft.gtd.camera;
 
+import java.awt.Window;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
-
-import org.apache.log4j.Logger;
 
 /**
- * A synthetic top-level parent for all AWT and FX (and HTML) gui components.
+ * A synthetic top-level parent for all Swing gui components.
  *
  * Created by Alaric on 14/07/2017.
  */
 public class Snapshot
 {
-	private final static transient Logger logger = Logger.getLogger( Snapshot.class );
-
 	@SuppressWarnings( "unchecked" )
 	public < T extends Object > List< T > getChildren()
 	{
-		List< T > children = new ArrayList<>();
-
-		children
-				.addAll( ( List< ? extends T > ) Arrays
-						.asList( java.awt.Window.getWindows() ) );
-
-		try
-		{
-			children
-					.addAll(
-							( List< ? extends T > ) com.sun.javafx.stage.StageHelper.getStages()
-									.stream()
-									.map( stage -> stage.getScene().getRoot() )
-									.collect( Collectors.toList() )
-					);
-		}
-		catch ( Throwable e )
-		{
-			logger.warn( e );
-
-			// java 9
-
-			// children.addAll(
-			// javafx.stage.Window.getWindows()
-			// .stream()
-			// .map( s -> s.getScene().getRoot() )
-			// .collect( Collectors.toList() )
-			// );
-		}
-
-		return children;
+		return new ArrayList<>( ( List< T > ) Arrays.asList( Window.getWindows() ) );
 	}
 }
